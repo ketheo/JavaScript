53b385bd1dc2b9a3323198a6555318b1543703d1
Karen Theodosopoulos <ketheo@gmail.com>
2022-12-27 19:54:26 -0500

    Java Scritp Methods and Arithmatics Changes

---
+++ JavaScriptCiscoTraining/JavaScriptMethods.js
@@ -0,0 +1,3 @@
+console.time();
+console.log("test console");
+console.timeEnd();
--- JavaScriptCiscoTraining/JavaScriptsArithmetic.js
+++ JavaScriptCiscoTraining/JavaScriptsArithmetic.js
@@ -6,13 +6,23 @@
 
 let testy = "100" - "10";
 console.log(testy);
+console.log(typeof testy);
 
 //The exception is the addition operation, which will not be treated as an arithmetic one, 
 //but as an attempt to create a new string by combining two input strings.
 
 let path = "C:\\" + "Windows";
 console.log(path);
+let path2 = "C:\\ Windows" - "Windows";
+console.log(path2);
 
-let test = '100' + '10';
-console.log(test);
-console.log(typeof test);
+//string interpolation. It allows you to treat a character string as a template, 
+//in which you can place values in selected places, such as those taken from variables. 
+//Such a literal is created using backticks (or grave accents) instead of quotation marks. 
+//The places where values are inserted are marked with curly brackets preceded by a $ sign.
+
+let country = 'Malawi';
+let continent = 'Afrca';
+
+let sentence = ` ${country} is located in ${continent}.`; //Use backtick NOT apostraphy.
+console.log(sentence);
